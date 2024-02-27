e2da2c13b2118fa28527ab6a3a4134af611bb52f
Reza1290 <reza.muktasib@gmail.com>
2024-02-27 16:51:36 +0700

    add: file

--- src/app/halodek/android/app/src/main/kotlin/com/example/halodek/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.halodek
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- src/app/heloworld/heloworld/android/app/src/main/kotlin/com/example/heloworld/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.heloworld
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
