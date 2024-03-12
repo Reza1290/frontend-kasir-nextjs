7f4ec99879493fd3d8ccb22f72a985d70d32b230
Reza1290 <reza.muktasib@gmail.com>
2024-03-12 21:09:30 +0700

    add: conf

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
     headers: [
         {
             key: 'Access-Control-Allow-Origin',
-            value: process.env.NEXT_PUBLIC_APP_URL,
+            value: "*",
         }
     ]
 };
